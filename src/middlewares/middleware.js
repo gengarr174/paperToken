e4ef5fb7ae68795b311a99c1a75190e0960ac212
Diego Rodrigues <srdiegorodrigues@gmail.com>
2026-03-19 14:43:24 -0300

    Inclusão de mais uma condição de verificação de erro de CSRF

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -16,15 +16,15 @@ export const csrfMidWare = (req,res,next)=>{
 }
 
 export const checkCsrfErr = (err,req,res,next)=>{
-    if(err && err.code ==="EBADCSRFTOKEN"){
-        return res.render("404")
+    if(err && (err.code ==="EBADCSRFTOKEN" || err.message === 'invalid csrf token')){
+        return res.status(404).render("404")
     }
     next();
 }
 
 export const globalMid = (req,res,next) =>{
     res.locals.errors = req.flash("errors");
-    res.locals.sucess = req.flash("success");
+    res.locals.success = req.flash("success");
     res.locals.user = req.session?.user || null;
     next();
 }
